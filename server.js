e61aa3ea368d2902617b8f029db402a58a7e5c06
nevor <nevikhun@gmail.com>
2020-05-08 15:02:07 -0300

    1

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const server = require("http").createServer(app);
+
+app.use(express.static(__dirname + "/client"));
+
+server.listen(process.env.PORT || 3000);
+console.log("Server running...")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//ROUTES
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/client/blank.html");
+})
+
+
