2bb9a4d245da49bd0619fce0d21dd8506676cd0a
nevor <nevikhun@gmail.com>
2020-05-10 10:45:58 -0300

    working on clinica db handler

--- handlers/db_handler.js
+++ handlers/db_handler.js
@@ -2,6 +2,7 @@ const PatientModel = require("../models/patientModel");
 const loginModule = require("./modules/login-module-db");
 const consultorioDermato = require("../models/dermatoModel");
 const consultorioEndocrino = require("../models/endocrinoModel");
+const consultorioClinica = require("../models/clinicaModel");
 
 
 const db_handler = {
@@ -17,6 +18,9 @@ const db_handler = {
     checkConsultorioEndocrino,
     enterEndocrinoWaitingLine,
     deleteMyEndocrinoTurn,
+    checkConsultorioClinica,
+    enterClinicaWaitingLine,
+    deleteMyClinicaTurn,
     login: loginModule
 }
 
@@ -129,6 +133,59 @@ async function checkConsultorioEndocrino(patient){
     }
  
 }
+
+//CLINICA
+async function enterClinicaWaitingLine(patient){
+    let created_patient;
+
+    try{
+        created_patient = await consultorioClinica.create(patient);
+        console.log("created")
+        return {denied: false};
+    }catch(error){
+        console.log("not created")
+        return {denied: true, error}
+    }
+ 
+}
+
+async function deleteMyClinicaTurn(patient){
+    let deleted;
+    try{
+        deleted = await consultorioClinica.findOneAndDelete(patient);
+        return deleted;
+    }catch(error){
+        return error
+    }
+}
+
+async function checkConsultorioClinica(patient){
+    let found = await consultorioClinica.find(patient);
+    let list = await consultorioClinica.find();
+
+    if (found.length === 0){
+        return false
+    } else {
+
+        if(list.length === 0){
+            return false;
+        } else {
+            //get index
+            let index = false;
+    
+            for(let i = 0; i < list.length; i++){
+                console.log(found.dni, list[i].dni)
+                if(found[0].dni === list[i].dni){
+                    index = i
+                } else {}
+            }
+
+            return {foundPatient: found, index: index}
+        }
+    }
+ 
+}
+
 
 async function findPatient(patient){
     let found = await PatientModel.find({email: patient.email, dni: parseInt(patient.dni)});
